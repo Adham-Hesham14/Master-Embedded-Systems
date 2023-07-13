fd9926685cc7567e6a1bef94571252c297117b7e
Adham Hesham <adhamhesham205@gmail.com>
2023-07-13 04:21:55 +0300

    pushing assigment of lab 1

---
+++ Unit3-Embedded C/Lesson2/Lab1/app.c
@@ -0,0 +1,8 @@
+# include"stdio.h"
+
+unsigned char buffer[100] ="Adham Hesham";
+
+void main(void){
+
+	send_string(buffer);
+}
---
+++ Unit3-Embedded C/Lesson2/Lab1/uart.h
@@ -0,0 +1,4 @@
+#ifndef _UART_H_
+#define _UART_H_
+void send_string(unsigned char* p_tx_string);
+#endif
