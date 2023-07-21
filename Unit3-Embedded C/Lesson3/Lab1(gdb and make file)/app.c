bbb424e4816f2ddea1d52da2c7d7368aaf56bd14
Adham Hesham <adhamhesham205@gmail.com>
2023-07-21 03:53:26 +0300

    pushing lab1(gdb and makefile)

---
+++ Unit3-Embedded C/Lesson3/Lab1(gdb and make file)/app.c
@@ -0,0 +1,9 @@
+# include"stdio.h"
+
+unsigned char buffer[100] ="Adham Hesham";
+const unsigned char buffer2[100] ="llllllllllll";
+const unsigned char buffer3[100] ="secoseco";
+void main(void){
+
+	send_string(buffer);
+}
